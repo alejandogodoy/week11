cc411e6f1068f7056617d0af7bf163d1cd6d25e6
Godoy Godoy <alejandogodoy22@gmail.com>
2021-04-19 14:21:37 -0300

    Add functions and cors to the index file

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = 4000;
+let user;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json());
+app.use(cors());
+
+function handleRegister (req, res) {
+    user = req.body;
+    res.status(200).send("success!");
+    console.log(user);
+}
+
+function handleLogin (req, res) {
+    let userPassword = req.body.password;
+    if (user != null && user.password == userPassword){
+        res.status(200).send("success!");
+    } 
+    else {
+        res.status(400).send("User not found");
+    }
+}
+
+app.post('/register' , handleRegister);
+app.put('/login' , handleLogin);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
